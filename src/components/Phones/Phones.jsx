99b50ae034de7dc384b7e7fd45c9c2b5e0ddd3e6
Anupama_Halder <anupamahalder2022@gmail.com>
2023-09-19 20:42:31 +0530

    Added a loading spinner from react awesome components

--- src/components/Phones/Phones.jsx
+++ src/components/Phones/Phones.jsx
@@ -3,10 +3,15 @@ import { useEffect, useState } from "react";
 import { BarChart, Bar } from "recharts";
 import { XAxis, YAxis } from 'recharts';
 import { Tooltip } from "recharts";
+import { Audio, Grid } from  'react-loader-spinner'
 
 const Phones = () => {
     // For loading data we need a state to hold data 
     const [phones, setPhones] = useState([]);
+
+    //declare a state to show loading spinner default is true when data is loading
+    const [loading, setLoading] = useState(true);
+
     //Loading data in asynchronous way, first time with empty dependency
     useEffect(() =>{
         // fetch('https://openapi.programming-hero.com/api/phones?search=iphone')
@@ -29,10 +34,35 @@ const Phones = () => {
             }) 
             console.log(phonesWithFakeData);
             setPhones(phonesWithFakeData);
+            //now we will setLoading to false as now loading is over
+            setLoading(false);
         })
     },[]);
     return (
         <div>
+            {/* To make conditional we will make it dynamic */}
+            {loading && <div>
+                <Audio
+                height="50"
+                width="50"
+                color="blue"
+                ariaLabel="audio-loading"
+                wrapperStyle={{}}
+                wrapperClass="wrapper-class"
+                visible={true}
+                />
+                <Grid
+                height="80"
+                width="80"
+                color="#4fa94d"
+                ariaLabel="grid-loading"
+                radius="12.5"
+                wrapperStyle={{}}
+                wrapperClass=""
+                visible={true}
+                />
+            </div>
+            }
             <h2 className="text-5xl">Phones: {phones.length}</h2>
             {/* Creating barchart with phone name and price data  */}
             <BarChart width={900} height={400} data={phones}>
